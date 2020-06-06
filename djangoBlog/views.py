3a42a03ee5bbf3f95c56653a83493436ab2e9303
MahsaMiresmaeili <mahsa.miresmaeili@yahoo.com>
2020-06-06 15:48:48 -0700

    create project

---
+++ djangoBlog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+def about(request):
+    return render(request,'about.html')
+
+
+def home(request):
+    return render(request,'Home.html')
+
+
+def mahsa(request):
+    return render(request,'mahsa.html')
+
+
